964709893e4b16a351f29e95f6a3148c89c8ba34
mrangel17 <yoangel17@gmail.com>
2017-07-31 08:59:53 -0500

    Consultas para el reportes de retencio

--- Model/GastoRepository.php
+++ Model/GastoRepository.php
@@ -33,9 +33,9 @@ class GastoRepository extends EntityRepository {
         $client = $stmt->fetchAll();
         return $client;
     }
-    
-    	function GetExpensesNoAffectBox($fecha, $usuario){
-$sql = "SELECT g.gas_valor AS cant1,
+
+    function GetExpensesNoAffectBox($fecha, $usuario) {
+        $sql = "SELECT g.gas_valor AS cant1,
                g.tiga_codigo,
                g.cod_caja,
                g.gas_codigo,
@@ -43,17 +43,35 @@ $sql = "SELECT g.gas_valor AS cant1,
                g.gas_afectaCaja, 
                tg.tiga_nombre 
                FROM gasto AS g, tipo_gasto AS tg 
-               WHERE g.gas_fecha='".$fecha."' 
-               AND usu_codigo = ".$usuario."
+               WHERE g.gas_fecha='" . $fecha . "' 
+               AND usu_codigo = " . $usuario . "
                AND gas_afectaCaja = 'No' 
-               AND g.tiga_codigo = tg.tiga_codigo";				
+               AND g.tiga_codigo = tg.tiga_codigo";
 //	        die($sql);
         $stmt = $this->getEntityManager()
                 ->getConnection()
                 ->prepare($sql);
         $stmt->execute();
         $client = $stmt->fetchAll();
-        return $client;		
-	}
+        return $client;
+    }
+
+    /**
+     * Esta fncion se encarga de seleccionar los datos de 
+     * retencion en la fuente con respecto a su ID,  necesarios para 
+     * calcularlos con los subtotaels de la compra
+     */
+    function SelectReteFontDataCode($codigo) {
+        $sql = "SELECT tm_codigo, tm_codigo_contable, tm_descripcion, tm_rete_uvt, tm_rete_tarifa, 
+        tm_rete_base_pesos, tm_rete_iva, tm_base_rete_ica, tm_tarifa_rete_ica FROM tipo_movimiento 
+        WHERE tm_codigo = '" . $codigo . "'";
+//die($sql);
+        $stmt = $this->getEntityManager()
+                ->getConnection()
+                ->prepare($sql);
+        $stmt->execute();
+        $client = $stmt->fetchAll();
+        return $client;
+    }
 
 }
--- Model/ProveedorRepository.php
+++ Model/ProveedorRepository.php
@@ -36,4 +36,68 @@ class ProveedorRepository extends EntityRepository {
         return $client;
     }
 
+    /**
+     *  Funcion que me permite obtener el listado de los proveedores a los cuales por la cantidad de compras que se le ha realizado
+     *  en un rango de fechas cumplen con el monto para efectuar retencion
+     *  @param $fechaInicio
+     *  @param $fechaFin
+     *  @return listadoproveedores
+     *  @author KIJHO TECHONOLOGIES S.A.S - Steven Salcedo 
+     *  @date 29 Ene 2014
+     *  @modificaciones 
+     */
+    function listSuppliersRetention($fechaInicio, $fechaFin, $proveedor) {
+        $where = '';
+        if ($proveedor > 0) {
+            $where = " AND fp.prov_codigo = " . $proveedor . "";
+        }
+        $sql = "SELECT SUM(fp.facp_valor) AS valor, 
+                      SUM(fp.facp_iva16) AS iva16, 
+                      SUM(fp.facp_iva10) AS iva10,
+                      SUM(fp.facp_iva5) AS iva5,
+                      SUM(fp.facp_reteFuente) AS retefuente,
+                      SUM(fp.facp_reteIca) AS reteIca, 
+                      fp.prov_codigo, 
+                      p.prov_nombre, 
+                      p.prov_nit 
+               FROM factura_proveedores fp, proveedor p 
+               WHERE fp.prov_codigo = p.prov_codigo 
+                     AND  fp.facp_fecha >= '" . $fechaInicio . "' 
+                     AND fp.facp_fecha <= '" . $fechaFin . "'
+                     " . $where . " 
+                     GROUP BY fp.prov_codigo";
+        $stmt = $this->getEntityManager()
+                ->getConnection()
+                ->prepare($sql);
+        $stmt->execute();
+        $client = $stmt->fetchAll();
+        return $client;
+    }
+    
+      /**
+     *  Funcion que me lista las facturas realizadas a un proveedor especifico, el cual se le efectuara retencion bajo un intervalo 
+     *  fechas establecidad por el usuario
+     *  @param $codProveedor
+     *  @param $fechaInicio
+     *  @param $fechaFin
+     *  @return listado Facturas
+     *  @author KIJHO TECHONOLOGIES S.A.S - Steven Salcedo 
+     *  @date 29 Ene 2014
+     *  @modificaciones 
+     */
+    function billsRetention($codProveedor, $fechaInicio, $fechaFin) {
+        $sql = "SELECT * FROM `factura_proveedores` 
+                WHERE prov_codigo = '" . $codProveedor . "' 
+                AND facp_fecha >= '" . $fechaInicio . "' 
+                AND facp_fecha <= '" . $fechaFin . "'";
+
+//           die($sql);
+             $stmt = $this->getEntityManager()
+                ->getConnection()
+                ->prepare($sql);
+        $stmt->execute();
+        $client = $stmt->fetchAll();
+        return $client;
+    }
+
 }
