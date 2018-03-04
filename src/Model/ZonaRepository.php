5a30343dc1b119538bf27924901b1844a1838eee
frcho <luisfer1g@gmail.com>
2018-03-04 13:37:59 -0500

    se corrige error en consulta para el repositorio de zonas

--- src/Model/ZonaRepository.php
+++ src/Model/ZonaRepository.php
@@ -8,17 +8,18 @@ use Doctrine\ORM\EntityRepository;
  * Description of ZonaRepository
  *
  * @author jocampo
+ * @author frcho
  */
 class ZonaRepository extends EntityRepository {
     public function getZonasByMunicipio($municipio_id){
-        $dql = "SELECT z FROM KijhoStructureBundle:Zona z JOIN  KijhoStructureBundle:Municipios m WITH mid = z.municipio "
+        $dql = "SELECT z FROM KijhoStructureBundle:Zona z JOIN  KijhoStructureBundle:Municipios m WITH m.id = z.municipio "
                 ."WHERE z.municipio = ".$municipio_id;
         $query = $this->getEntityManager()->createQuery($dql);
         return $query->getResult();
     }
     
     public function getZonasByRuta($ruta_id){
-        $dql = "SELECT z FROM KijhoStructureBundle:Zona z JOIN  KijhoStructureBundle:Ruta r WITH rid = z.ruta "
+        $dql = "SELECT z FROM KijhoStructureBundle:Zona z JOIN  KijhoStructureBundle:Ruta r WITH r.id = z.ruta "
                 ."WHERE z.ruta = ".$ruta_id;
         $query = $this->getEntityManager()->createQuery($dql);
         return $query->getResult();
